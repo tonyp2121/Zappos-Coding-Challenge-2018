475b31837c66e0c5fcaef2541a3e1363d56e4e0b
Anthony Pallone <pallone@unlv.nevada.edu>
2018-03-06 19:38:12 -0800

    Initial Commit

---
+++ zappos.cpp
@@ -0,0 +1,55 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+void errorCode();
+
+// should probably make chef class
+class chef
+{
+    public:
+        int MPM;
+        int Q;
+        string Z;
+        string F;
+    private:
+
+}
+
+using namespace std;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+    // variables
+    int K; //  K number of chefs
+    int N; // team members
+
+    cin >> K;
+    if(K < || K > 100){errorCode();}
+    chef chefs[];
+
+    return 0;
+}
+
+void errorCode(){
+    cout << "there was an error";
+}
